4de351e5b5ed59d39f01b530da1ef4bbb97bcdc6
Chris Pawlukiewicz <hi@paynoattn.com>
2023-01-09 08:48:48 -0700

    angular

---
+++ angular/projects/web-lightning/src/main.ts
@@ -0,0 +1,9 @@
+import { loadManifest } from '@angular-architects/module-federation';
+
+loadManifest("./assets/mf.manifest.json")
+    .catch(err => console.error(err))
+    .then(_ => import('./bootstrap'))
+    .catch(err => {
+        console.info('asdf', err);
+        import('./bootstrap')
+    });
---
+++ angular/projects/web-lightning/src/mfe-declaration.d.ts
@@ -0,0 +1,2 @@
+
+declare module 'marketplace/Module';
